7dc1a46f48b1c97aba53295ceeedd43d2f8012cb
Sabrina <sizcovich@users.noreply.github.com>
2013-10-21 02:45:48 +0000

    subi todo

---
+++ codigo/compilar.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#g++ -std=c++0x -o ./ej1/ej1 ./ej1/ej1.cpp -lrt
+ g++ -std=c++0x -o ./ej2/ej2.exe ./ej2/ej2.cpp -lrt
+# g++ -std=c++0x -o  ./ej3/ej3.exe ./ej3/ej3.cpp -lrt
+
+#cat ./ej1/ej1.in | ./ej1/ej1 #1> ./ej1/ej1.out 2> ./ej1/ej1.dat
+ cat ./ej2/ej2.in | ./ej2/ej2.exe # 1> ./ej2/ej2.out 2> ./ej2/ej2.dat
+# cat ./ej3/ej3.in | ./ej3/ej3.exe # 1> ./ej3/ej3.out 2> ./ej3/ej3.dat
---
+++ codigo/ej3/heuristicaGolosa.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <cstdio>
+#include <chrono>
+#include <iostream>
+#include <algorithm>
+#include <vector> // http://www.cplusplus.com/reference/vector/vector/
+#include <queue>
+#include <ctgmath>
+
+using namespace std; 
+
+
+int main() {
+
+	return 0;
+}
