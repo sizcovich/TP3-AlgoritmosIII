d15c7bb687f827397a41a09639c15ef8a7147ebc
sebastianvita <sevita@users.noreply.github.com>
2013-11-03 01:43:29 +0000

    heuristica local

--- codigo/ej3/heuristicaBusquedaLocal.cpp
+++ codigo/ej3/heuristicaBusquedaLocal.cpp
@@ -37,6 +37,36 @@ void escribirOutput(ostream& os, const vuint& solucion) {
 	cout << endl;
 }
 
+pair <int,vector<int> > mejorVecinaQuitando(grafo, s0) { //quito el de menor grado
+	fronteraClique = frontera(grafo, s0);
+	
+	int minimo = grafo.vecindad(s0[0]).size(); //seteo un grado
+	int minimoNodo = 0;
+	int aux;
+	
+	for (int i = 1; i < s0.size(); i++){ // calculo cual es el nodo de s0 de menor grado
+		aux = grafo.vecindad(s0[i]).size();
+		if (aux < minimo){
+			minimo = aux
+			minimoNodo = i;
+		}
+	}
+	
+	vector<int> cliqueQuitando;
+	for (int i = 0; i < s0.size(); i++){
+		if(minimoNodo != i)
+			cliqueQuitando.push_back(s0[i]);
+	}
+	
+	minimo -= (s0.size() - 1); //#aristas de la frontera q aportaba ese nodo
+	fronteraClique -= minimo; //quito la #aristas de la frontera q aportaba el nodo eliminado
+	fronteraClique += (s0.size() - 1);
+	
+	pair <int,vector<int> > res;
+	res = make_pair (fronteraClique,cliqueQuitando);
+	return res;	
+}
+
 int main() {
 	Grafo grafo = leerInput(cin);
 	
@@ -44,49 +74,34 @@ int main() {
 		
 		//represento a la clique como un vector de nodos que perteneces a la clique
 		vector<int> s0(1,1); //creo la clique con un solo nodo (el 1); todo grafo no vacio tiene por lo menos un nodo
-		vector<int> cliqueAgregando;
-		vector<int> cliqueQuitando;
-		vector<int> cliquePermutando;
-		
+	
 		int tamFrontera = frontera(grafo, s0);
 		
 		while(true){
-			int aux = 0; //mejor vecina 
-			int maximo = 0; //maximo hasta ahora de los vecinos
+			pair <int,vector<int> > aux; //mejor vecina 
+			pair <int,vector<int> > maximo; //maximo hasta ahora de los vecinos
 			int maximaClique = 0; // 0 = no habia cliques; 1 = agregado; 2 = quitando; 3 = permutando
 			
 			// genero las vecindades y devuelvo la mejor
-			maximo = mejorVecinaAgregando(grafo, s0, cliqueAgregando);
-			if (maximo != 0)
+			maximo = mejorVecinaAgregando(grafo, s0);
+			if (maximo.first() != 0)
 				maximaClique = 1;
 				
-			aux = mejorVecinaQuitando(grafo, s0, cliqueQuitando);
-			if (aux > maximo){
+			aux = mejorVecinaQuitando(grafo, s0);
+			if (aux.first() > maximo.first()){
 				maximo = aux;
 				maximaClique = 2;
 			}
 				
-			aux = mejorVecinaPermutando(grafo, s0, cliquePermutando);
-			if (aux > maximo){
+			aux = mejorVecinaPermutando(grafo, s0);
+			if (aux.first() > maximo.first()){
 				maximo = aux;
 				maximaClique = 3;
 			}
 			
-			if(maximo > tamFrontera){
-				tamFrontera = maximo;
-				switch (maximaClique)
-				{
-					case 1:
-						s0 = cliqueAgregando;
-						break;
-					
-					case 2:
-						s0 = cliqueQuitando;
-						break;	
-						
-					default:
-						s0 = cliquePermutando;
-				}
+			if(maximo.first() > tamFrontera){
+				tamFrontera = maximo.first();
+				s0 = maximo.second();
 			}
 			else
 				break;
