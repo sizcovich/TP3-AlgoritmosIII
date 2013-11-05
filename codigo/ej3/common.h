3d387462cb31ad469857659569acec08e3e0397b
Sabrina <sizcovich@users.noreply.github.com>
2013-11-05 00:20:43 +0000

    common

---
+++ codigo/ej3/common.h
@@ -0,0 +1,13 @@
+#ifndef __AED3__COMMON__
+#define __AED3__COMMON__
+
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+typedef unsigned int uint;
+typedef std::vector<uint> vuint;
+
+#define FORN(i,n) for(uint i = 0; i < n; ++i)
+
+#endif
