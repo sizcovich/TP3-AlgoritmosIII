7cf384d5359fe321558101614829eec654369358
iorellana <iorellana@153f3cea-87ef-42a7-9c34-acf9064337c8>
2013-11-14 23:45:12 +0000

    asd

--- codigo/compilar.sh
+++ codigo/compilar.sh
@@ -2,7 +2,7 @@
 
 #g++ -std=c++0x -o ./ej1/ej1 ./ej1/ej1.cpp -lrt
 # g++ -std=c++0x -o ./ej2/ej2.exe ./ej2/ej2.cpp -lrt
- g++ -std=c++0x -o  ./ej3/busquedaTabu.exe ./ej3/busquedaTabu.cpp -lrt
+ # g++ -std=c++0x -o  ./ej3/busquedaTabu.exe ./ej3/busquedaTabu.cpp -lrt
 
 #cat ./ej1/ej1.in | ./ej1/ej1 #1> ./ej1/ej1.out 2> ./ej1/ej1.dat
 # cat ./ej2/ej2.in | ./ej2/ej2.exe # 1> ./ej2/ej2.out 2> ./ej2/ej2.dat
