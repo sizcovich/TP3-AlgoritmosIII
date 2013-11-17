cda004fd00e92d95ef523a98e46780a0ca96cec6
magarcia <magarcia@153f3cea-87ef-42a7-9c34-acf9064337c8>
2013-11-17 00:47:49 +0000

    pepe

--- codigo/compilar.sh
+++ codigo/compilar.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-#g++ -std=c++0x -o ./ej1/ej1 ./ej1/ej1.cpp -lrt
-# g++ -std=c++0x -o ./ej2/ej2.exe ./ej2/ej2.cpp -lrt
- g++ -std=c++0x -o  ./ej3/busquedaTabu.exe ./ej3/busquedaTabu.cpp -lrt
+ #g++ -std=c++0x -o ./ej3/heuristicaGolosa.exe ./ej3/heuristicaGolosa.cpp -lrt
+ #g++ -std=c++0x -o ./ej3/heuristicaBusquedaLocal.exe ./ej3/heuristicaBusquedaLocal.cpp -lrt
+ #g++ -std=c++0x -o  ./ej3/busquedaTabu.exe ./ej3/busquedaTabu.cpp -lrt
+  g++ -std=c++0x -o  ./ej3/heuristicas.exe ./ej3/heuristicas.cpp -lrt
 
-#cat ./ej1/ej1.in | ./ej1/ej1 #1> ./ej1/ej1.out 2> ./ej1/ej1.dat
-# cat ./ej2/ej2.in | ./ej2/ej2.exe # 1> ./ej2/ej2.out 2> ./ej2/ej2.dat
- cat ./ej3/ej3.in | ./ej3/busquedaTabu.exe # 1> ./ej3/ej3.out 2> ./ej3/ej3.dat
+ #cat ./ej3/ej3.in | ./ej3/heuristicaGolosa.exe #1> ./ej3/heuristicaGolosa.out 2> ./ej3/heuristicaGolosa.dat
+ #cat ./ej3/ej3.in | ./ej3/heuristicaBusquedaLocal.exe # 1> ./ej3/heuristicaLocal.out 2> ./ej3/heuristicaLocal.dat
+ #cat ./ej3/ej3.in | ./ej3/busquedaTabu.exe # 1> ./ej3/heuristicaTabu.out 2> ./ej3/heuristicaTabu.dat
+  cat ./ej3/ej3.in | ./ej3/heuristicas.exe
