2f891526cc8957b237a5870c5b7b1ad2ee8309da
iorellana <iorellana@153f3cea-87ef-42a7-9c34-acf9064337c8>
2013-11-22 20:40:51 +0000

    asd

--- codigo/tests/testCalidadGrande.sh
+++ codigo/tests/testCalidadGrande.sh
@@ -21,23 +21,25 @@ TABU="../ej3/busquedaTabu"
 
 echo "Corriendo test de calidad de la solucion para algoritmos grades: "
 echo "Compilando..."
-rm ../ej3/heuristicaGolosa
-g++ -std=c++0x ../ej3/heuristicaGolosa.cpp -o ../ej3/heuristicaGolosa
-rm ../ej3/heuristicaBusquedaLocal
-g++ -std=c++0x ../ej3/heuristicaBusquedaLocal.cpp -o ../ej3/heuristicaBusquedaLocal
-# rm ../ej3/busquedaTabu
-# g++ -std=c++0x ../ej3/busquedaTabu.cpp -o ../ej3/busquedaTabu
+# rm ../ej3/heuristicaGolosa
+# g++ -std=c++0x ../ej3/heuristicaGolosa.cpp -o ../ej3/heuristicaGolosa
+# rm ../ej3/heuristicaBusquedaLocal
+# g++ -std=c++0x ../ej3/heuristicaBusquedaLocal.cpp -o ../ej3/heuristicaBusquedaLocal
+rm $TABU
+g++ -std=c++0x ../ej3/busquedaTabu.cpp -o $TABU
+
+rm calidadGrande/tabu_*.dat
 
 for i in $TIPOS_DE_GRAFOS; do
 	echo "Creando grafos de tipo $i..."
 	rm $INPUT_FILE_GEN
 	./graph_generator $i $CANT_NODOS_MIN $CANT_NODOS_MAX $INPUT_FILE_GEN $QUANT_PER_SIZE $INCREMENT
-	echo "Corriendo goloso.."
-	cat $INPUT_FILE_GEN | $GOLOSO 1> /dev/null 2>> calidadGrande/goloso_$i.dat
-	echo "Corriendo local.."
-	cat $INPUT_FILE_GEN | $LOCAL 1> /dev/null 2>> calidadGrande/local_$i.dat
-	# echo "Corriendo tabu.."
-	# cat $INPUT_FILE_GEN | $TABU 1>> tabu_$i.out 2>> tabu_$i.dat
+	# echo "Corriendo goloso.."
+	# cat $INPUT_FILE_GEN | $GOLOSO 1> /dev/null 2>> calidadGrande/goloso_$i.dat
+	# echo "Corriendo local.."
+	# cat $INPUT_FILE_GEN | $LOCAL 1> /dev/null 2>> calidadGrande/local_$i.dat
+	echo "Corriendo tabu.."
+	cat $INPUT_FILE_GEN | $TABU 1>> /dev/null 2>> calidadGrande/tabu_$i.dat
 	echo "Listo!"
 done
 
