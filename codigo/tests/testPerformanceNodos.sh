f8aba25536374018676cdeb61173d476631a602b
iorellana <iorellana@users.noreply.github.com>
2013-12-18 04:18:10 +0000

    asd

--- codigo/tests/testPerformanceNodos.sh
+++ codigo/tests/testPerformanceNodos.sh
@@ -38,12 +38,12 @@ g++ -std=c++0x -O2 ../ej3/heuristicaBusquedaLocal.cpp -o ../ej3/heuristicaBusque
 # g++ -std=c++0x ../ej3/busquedaTabu.cpp -o ../ej3/busquedaTabu
 
 rm $INPUT_FILE_GEN
-# echo "Creando grafos de tipo completo..."
-# ./graph_generator 4 $CANT_NODOS_MIN $CANT_NODOS_MAX $INPUT_FILE_GEN $QUANT_PER_SIZE $INCREMENT $DENSITY
-# echo "Corriendo goloso.."
-# cat $INPUT_FILE_GEN | $GOLOSO 1>> /dev/null 2>> performance/goloso_4_prueba.dat
-# echo "Corriendo local.."
-# cat $INPUT_FILE_GEN | $LOCAL 1>> /dev/null 2>> performance/local_4.dat
+echo "Creando grafos de tipo completo..."
+./graph_generator 4 $CANT_NODOS_MIN $CANT_NODOS_MAX $INPUT_FILE_GEN $QUANT_PER_SIZE $INCREMENT $DENSITY
+echo "Corriendo goloso.."
+cat $INPUT_FILE_GEN | $GOLOSO 1>> /dev/null 2>> performance/goloso_4_prueba.dat
+echo "Corriendo local.."
+cat $INPUT_FILE_GEN | $LOCAL 1>> /dev/null 2>> performance/local_4.dat
 
 # echo "Corriendo tabu.."
 # rm performance/tabu_4.dat
@@ -51,12 +51,12 @@ rm $INPUT_FILE_GEN
 # echo "Listo!"
 
 # rm $INPUT_FILE_GEN
-echo "Creando grafos de tipo conexo..."
-./graph_generator 12 $CANT_NODOS_MIN $CANT_NODOS_MAX $INPUT_FILE_GEN $QUANT_PER_SIZE $INCREMENT "0.5"
-echo "Corriendo goloso.."
-cat $INPUT_FILE_GEN | $GOLOSO 1>> /dev/null 2>> performance/goloso_12.dat
-echo "Corriendo local.."
-cat $INPUT_FILE_GEN | $LOCAL 1>> /dev/null 2>> performance/local_12.dat
+# echo "Creando grafos de tipo conexo..."
+# ./graph_generator 12 $CANT_NODOS_MIN $CANT_NODOS_MAX $INPUT_FILE_GEN $QUANT_PER_SIZE $INCREMENT "0.5"
+# echo "Corriendo goloso.."
+# cat $INPUT_FILE_GEN | $GOLOSO 1>> /dev/null 2>> performance/goloso_12.dat
+# echo "Corriendo local.."
+# cat $INPUT_FILE_GEN | $LOCAL 1>> /dev/null 2>> performance/local_12.dat
 # echo "Corriendo tabu.."
 # cat $INPUT_FILE_GEN | $TABU 1>> /dev/null 2>> performance/tabu_12.dat
 echo "Listo!"
